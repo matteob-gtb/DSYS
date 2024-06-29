4e69a3fa2c2c5e55df41823c2aa34fcdbf136091
mb <matteobevacqua@outlook.com>
2024-06-29 11:31:43 +0200

    fixed welcome

--- src/Messages/AnonymousMessages/WelcomeMessage.java
+++ src/Messages/AnonymousMessages/WelcomeMessage.java
@@ -2,12 +2,14 @@ package Messages.AnonymousMessages;
 
 import Messages.AbstractMessage;
 import com.google.gson.Gson;
+import utils.Constants;
 
 public class WelcomeMessage extends AbstractMessage {
 
     public WelcomeMessage(int clientID,String username) {
         this.senderID = clientID;
         this.username = username;
+        this.messageType = Constants.MESSAGE_TYPE_WELCOME;
     }
 
 
